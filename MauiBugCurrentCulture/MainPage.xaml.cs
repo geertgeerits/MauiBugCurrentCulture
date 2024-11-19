ceafa5dc5fe8bbc91c8dffe32e8676d0755036a1
Geert Geerits <ggeerits@hotmail.com>
2024-11-19 21:08:50 +0100

    Add project files.

---
+++ MauiBugCurrentCulture/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+
+namespace MauiBugCurrentCulture
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+            lblCurrentCulture.Text = "CurrentCulture:  " + CultureInfo.CurrentCulture.Name;
+
+            Debug.WriteLine($"CultureInfo.CurrentCulture.Name: {CultureInfo.CurrentCulture.Name}");
+        }
+    }
+}
